3930831aeffc6aa5dd778b5fe46aef74e2e1492b
Phil Crosby <phil.crosby@gmail.com>
2013-03-02 19:09:30 -0800

    Indentation

--- shoulda.js
+++ shoulda.js
@@ -91,13 +91,11 @@ scope.assert = {
     else if (object === undefined) return "undefined";
     else if (typeof object === "string") return '"' + object + '"';
     else {
-        try {
-            return JSON.stringify(object);
-        }
-        catch (exception) {
-            // object might not be stringifiable (e.g. DOM nodes), or JSON.stringify may not exist
-            return object.toString();
-        }
+      try { return JSON.stringify(object); }
+      catch (exception) {
+        // object might not be stringifiable (e.g. DOM nodes), or JSON.stringify may not exist.
+        return object.toString();
+      }
     }
   }
 };
